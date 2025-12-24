d478d7289ea40a79cf96a82b902117e77ecdab79
Landon Tally <landonraytally+1@gmail.com>
2025-12-24 09:21:56 -0500

    Updated nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -167,7 +167,7 @@
                                 <span class="w-8 h-8 rounded-full bg-white/5 flex items-center justify-center group-hover:bg-brand-sky group-hover:text-brand-navy transition-colors">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" /></svg>
                                 </span>
-                                ljkamen@gmail.com
+                                
                             </a>
 
                              <div class="flex items-start gap-4 text-slate-300 group">
